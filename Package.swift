854bb5b72f6e6ec0d4f14023a3a33c63379d5f70
bala obulreddy <balaobulreddy508@gmail.com>
2024-07-10 19:03:38 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -29,14 +29,12 @@ let package = Package(
       .binaryTarget(
           name: "WebexConnectPush",
           url: "https://github.com/balaobulreddy/modularizedsdk/releases/download/2.20.0/WebexConnectPush.zip",
-          checksum: "e567c012f801c0faed150832fd3e74360e81830e8330f79908e9a4a629f1a4b1",
-          dependencies: ["WebexConnectCore"]
+          checksum: "e567c012f801c0faed150832fd3e74360e81830e8330f79908e9a4a629f1a4b1"
       ),
     .binaryTarget(
           name: "WebexConnectInAppMessaging",
           url: "https://github.com/balaobulreddy/modularizedsdk/releases/download/2.20.0/WebexConnectInAppMessaging.zip",
-          checksum: "c7c57c35bed1db2ec1f4771a27cc127f5c76b871c775d9440c821bbd762d9e20",
-          dependencies: ["WebexConnectCore"]
+          checksum: "c7c57c35bed1db2ec1f4771a27cc127f5c76b871c775d9440c821bbd762d9e20"
       ),
       .binaryTarget(
           name: "WebexConnectNotificationServiceExtension",
