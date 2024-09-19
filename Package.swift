cb5ee4ad2439c5137caa8c5aa896dfdb6cd80d63
bala obulreddy <balaobulreddy508@gmail.com>
2024-09-19 18:28:36 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   targets: [
       .binaryTarget(
           name: "WebexConnectCoreSDKFull",
-          url: "https://github.com/webex/wxconnect-core-ios-sdk/releases/download/2.19.6/WebexConnectCoreSDKFull_SPM.zip",
+          url: "https://github.com/balaobulreddy/balatestnew1/releases/download/2.0.2/GitRepoNameTest2.zip",
           checksum: "64b598701092f50197f64670aeb3ce9a827e383e9cd09fe8b58be9f2e872789b"
       )
   ]
